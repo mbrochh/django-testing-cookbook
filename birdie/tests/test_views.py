d532653f93e2ca72b0735b03fba4ac985e4e8205
Martin Brochhaus <mbrochh@gmail.com>
2016-06-08 07:50:50 +0800

    Added AdminView & template & tests

--- birdie/tests/test_views.py
+++ birdie/tests/test_views.py
@@ -1,4 +1,8 @@
+import pytest
+from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory
+from mixer.backend.django import mixer
+pytestmark = pytest.mark.django_db
 
 from .. import views
 
@@ -8,3 +12,18 @@ class TestHomeView:
         req = RequestFactory().get('/')
         resp = views.HomeView.as_view()(req)
         assert resp.status_code == 200, 'Should be callable by anyone'
+
+
+class TestAdminView:
+    def test_anonymous(self):
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        resp = views.AdminView.as_view()(req)
+        assert 'login' in resp.url, 'Should redirect to login'
+
+    def test_superuser(self):
+        user = mixer.blend('auth.User', is_superuser=True)
+        req = RequestFactory().get('/')
+        req.user = user
+        resp = views.AdminView.as_view()(req)
+        assert resp.status_code == 200, 'Should be callable by superuser'
--- birdie/views.py
+++ birdie/views.py
@@ -1,5 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views import generic
 
 
 class HomeView(generic.TemplateView):
     template_name = 'birdie/home.html'
+
+
+class AdminView(generic.TemplateView):
+    template_name = 'birdie/admin.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(AdminView, self).dispatch(request, *args, **kwargs)
