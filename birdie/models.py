af5d0850d4de074db24b5837274ad11422c72cd9
Martin Brochhaus <mbrochh@gmail.com>
2016-06-07 08:55:43 +0800

    Added Post.get_excerpt() function & test

--- birdie/models.py
+++ birdie/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Post(models.Model):
     body = models.TextField()
+
+    def get_excerpt(self, chars):
+        return self.body[:chars]
--- birdie/tests/test_models.py
+++ birdie/tests/test_models.py
@@ -7,3 +7,10 @@ class TestPost:
     def test_init(self):
         obj = mixer.blend('birdie.Post')
         assert obj.pk == 1, 'Should save an instance'
+
+    def test_get_excerpt(self):
+        obj = mixer.blend('birdie.Post', body='Hello World!')
+        result = obj.get_excerpt(5)
+        expected = 'Hello'
+        assert result == expected, (
+            'Should return the given number of characters')
